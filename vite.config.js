f4e5e6be8bec6926c7a9616fc60e65325d6c1929
youssef <youssefnabil2295@gmail.com>
2026-06-12 18:30:48 +0300

    Fix base path for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/e-commerce/',
+  base: '/',
   esbuild: {
     loader: 'jsx',
     include: /src\/.*\.[jt]sx?$/,
